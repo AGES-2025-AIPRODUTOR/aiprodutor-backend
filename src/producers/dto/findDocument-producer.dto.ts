941934190f081de6aeddc58be34445806a58f6f1
gugab <gustavobpasquali2@gmail.com>
2025-09-06 19:17:21 -0300

    fix: optional on swagger

--- src/producers/dto/findDocument-producer.dto.ts
+++ src/producers/dto/findDocument-producer.dto.ts
@@ -1,18 +1,20 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import {
     Matches,
-    IsNumberString
+    IsNumberString,
+    IsOptional
 
 } from 'class-validator';
 
 export class FindDocumentoProducerDto {
-@ApiProperty({
+@ApiPropertyOptional({
     description: 'O CPF ou CNPJ do produtor (somente números, sem formatação).',
     example: '12345678901',
   })
 @IsNumberString({}, {
     message:  'document deve conter apenas números.'
   })
+@IsOptional()
 @Matches(/^(\d{11}|\d{14})$/, {
     message: 'document deve ser um CPF (11 dígitos) ou CNPJ (14 dígitos) válido, somente números.',
   })
