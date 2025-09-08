4e7d9329f52bb4934a1829d6c9aa6c5ba46f628c
Lucas Ramon <lucas.ramon@edu.pucrs.br>
2025-09-08 11:09:54 -0300

    chore: atualiza configurações globais da aplicação e dependências

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-// src/main.ts
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -7,21 +5,30 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1/');
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
-  // Configura o Swagger
-  const config = new DocumentBuilder()
+  // Define o prefixo global PRIMEIRO
+  app.setGlobalPrefix('api/v1', {
+    exclude: ['/'], // Exclui a rota raiz do prefixo
+  });
+
+
+  const config = new DocumentBuilder() // Cria a configuração do Swagger
     .setTitle('API Ai Produtor')
     .setDescription(
-      'Documentação da API para o sistema de gestão de produtores',
+      'Documentação da API para o sistema de gestão de produtores', 
     )
     .setVersion('1.0')
+    // A linha abaixo informa à interface do Swagger qual o prefixo do servidor
+    .addServer('/api/v1') 
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+
+bootstrap().catch((err) => console.error(err));
