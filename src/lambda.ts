0c77ca5d03306974af6a1f7d9815d782b7de5fa6
Lucas Ramon <lucas.ramon@edu.pucrs.br>
2025-10-10 17:58:39 -0300

    Configura pipeline GitLab Runner na AWS

---
+++ src/lambda.ts
@@ -0,0 +1,28 @@
+// src/lambda.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as serverless from 'serverless-http'; // Correct way to import
+import { Handler, Context, Callback } from 'aws-lambda';
+import { ValidationPipe } from '@nestjs/common';
+
+let cachedServer: Handler;
+
+async function bootstrap(): Promise<Handler> {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
+    app.setGlobalPrefix('api/v1', { exclude: ['/'] });
+
+    await app.init();
+    const expressApp = app.getHttpAdapter().getInstance();
+    cachedServer = serverless(expressApp);
+  }
+  return cachedServer;
+}
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  const server = await bootstrap();
+  return server(event, context, callback);
+};
