17f96a2d7362d536ac990e4b990ad545dba1482b
Felipe R. Tasoniero <frtasoniero@gmail.com>
2025-11-14 13:17:08 -0300

    Remove conflicting migrations after restoring features

--- prisma/migrations/20251026045928_unique_producer_name/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[producerId,name]` on the table `products` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "products_producerId_name_key" ON "public"."products"("producerId", "name");
